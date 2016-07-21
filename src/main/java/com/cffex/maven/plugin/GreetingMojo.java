c88b890001f999cdad7980049498541527dcc745
Chen.Ming <uchenm@gmail.com>
2016-07-21 09:36:25 +0800

    param

--- src/main/java/com/cffex/maven/plugin/GreetingMojo.java
+++ src/main/java/com/cffex/maven/plugin/GreetingMojo.java
@@ -3,6 +3,7 @@ package com.cffex.maven.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Created by Ming on 2016/7/20.
@@ -10,8 +11,15 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo( name = "sayhi")
 public class GreetingMojo extends AbstractMojo
 {
+
+    /**
+     * The greeting to display.
+     */
+    @Parameter( property = "sayhi.greeting", defaultValue = "Hello World!" )
+    private String greeting;
+
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Hello, world." );
+        getLog().info( greeting);
     }
 }
